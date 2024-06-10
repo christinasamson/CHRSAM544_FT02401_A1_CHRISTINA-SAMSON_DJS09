211a2ad0898a1658d8d32eaee0c7338147eb0540
christinasamson <156719832+christinasamson@users.noreply.github.com>
2024-06-10 10:08:13 +0200

    modified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,39 @@
+const path = require('path');
+
 module.exports = {
-  "output": {
-    "filename": "[name].pack.js"
-  },
-  "entry": {
-    "index": "./index"
-  },
-  "resolve": {
-    "extensions": [
-      ".ts",
-      ".js",
-      ".json"
-    ]
-  },
-  "module": {
-    "rules": [
-      {
-        "use": {
-          "loader": "ts-loader"
-        },
-        "exclude": /node_modules/,
-        "test": /\.ts$/
-      }
-    ]
-  }
+	mode: 'development', // or 'production'
+	entry: {
+		index: './src/index.ts',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'), // Ensure output path is defined
+		filename: '[name].pack.js',
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		compress: true,
+		port: 9000,
+	},
 };
